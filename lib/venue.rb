295ca804331b60399ba89f6e98e73aedbfe6690e
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-01-30 10:06:32 -0800

    Capitalize the first letter of the venue name.

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,4 +1,10 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many(:bands)
   validates(:venue, {:presence => true})
+  before_save(:capitalize_venue)
+
+private
+  define_method(:capitalize_venue) do
+    self.venue=(venue().capitalize())
+  end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,8 +2,14 @@ require('spec_helper')
 
 describe(Venue) do
   it {should have_and_belong_to_many (:bands)}
+
   it("validates presence of venue name") do
     venue = Venue.new({:venue => ''})
     expect(venue.save()).to(eq(false))
+  end
+
+  it("will capitalize the first letter of the venue") do
+    venue = Venue.create({:venue => "refuge pdx"})
+    expect(venue.venue()).to(eq("Refuge pdx"))
   end
 end
