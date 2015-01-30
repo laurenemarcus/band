36e8eef172df0b555e9ab822d4a102d80e52b387
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-01-30 10:43:30 -0800

    Allows user to navigate to the band page and to the page where venues can be selected, the band name can be updated and deleted.

--- app.rb
+++ app.rb
@@ -5,3 +5,27 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file}
+
+get("/") do
+  @bands = Band.all()
+  @venues = Venue.all()
+  erb(:index)
+end
+
+get("/bands") do
+  @bands = Band.all()
+  erb(:band)
+end
+
+post("/bands") do
+  band = params.fetch("band")
+  @bands = Band.all()
+  Band.create({:band => band})
+  erb(:band)
+end
+
+get("/bands/:id") do
+  @band = Band.find(params.fetch("id").to_i())
+  @venues = Venue.all()
+  erb(:band_info)
+end
