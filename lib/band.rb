d4e424edca270c3b6b279f52590abb36c5999f49
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-01-30 09:53:58 -0800

    Not allow user to enter blank band name.

--- lib/band.rb
+++ lib/band.rb
@@ -1,3 +1,4 @@
 class Band < ActiveRecord::Base
-  has_and_belongs_to_many(:venues)  
+  has_and_belongs_to_many(:venues)
+  validates(:band, {:presence => true}) 
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,4 +2,8 @@ require('spec_helper')
 
 describe(Band) do
   it {should have_and_belong_to_many (:venues)}
+  it("validates presence of band name") do
+    band = Band.new({:band => ''})
+    expect(band.save()).to(eq(false))
+  end
 end
