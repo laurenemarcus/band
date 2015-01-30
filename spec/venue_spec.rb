144c4bd0f12c951645d82e354d1af44d859a648c
Lauren Marcus <lauren.e.marcus@gmail.com>
2015-01-30 10:03:56 -0800

    Not allow user to enter a blank venue name.

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,4 +2,8 @@ require('spec_helper')
 
 describe(Venue) do
   it {should have_and_belong_to_many (:bands)}
+  it("validates presence of venue name") do
+    venue = Venue.new({:venue => ''})
+    expect(venue.save()).to(eq(false))
+  end
 end
